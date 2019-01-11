7ac83a56ae7b27a4b257f3181cf265bf1f9b454a
Mayank Sanganeria <mayank.sanganeria@leanplum.com>
2019-01-11 10:45:55 -0800

    remove pods from git

--- Pods/Target Support Files/Leanplum-iOS-SDK/Leanplum-iOS-SDK-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Leanplum_iOS_SDK : NSObject
-@end
-@implementation PodsDummy_Leanplum_iOS_SDK
-@end
--- Pods/Target Support Files/Pods-Rondo-iOS/Pods-Rondo-iOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Rondo_iOS : NSObject
-@end
-@implementation PodsDummy_Pods_Rondo_iOS
-@end
