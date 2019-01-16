c761569f66141550608d41ae0011f3a63ccb6d9b
Mayank Sanganeria <mayank.sanganeria@leanplum.com>
2019-01-15 17:39:41 -0800

    clear badge number

--- Rondo-iOS/AppDelegate.m
+++ Rondo-iOS/AppDelegate.m
@@ -17,6 +17,7 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    [[UIApplication sharedApplication] setApplicationIconBadgeNumber:0];
     return YES;
 }
 
