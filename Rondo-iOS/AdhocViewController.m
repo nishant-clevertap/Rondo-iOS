af661104f803f941f01b6627e14659a614a24230
Mayank Sanganeria <mayank.sanganeria@leanplum.com>
2019-01-15 17:14:19 -0800

    persist last used event names

--- Rondo-iOS/AdhocViewController.m
+++ Rondo-iOS/AdhocViewController.m
@@ -9,6 +9,9 @@
 #import "AdhocViewController.h"
 #import <Leanplum/Leanplum.h>
 
+#define ADHOC_VIEW_CONTROLLER_PERSIST_TRACK_KEY @"ADHOC_VIEW_CONTROLLER_PERSIST_TRACK_KEY"
+#define ADHOC_VIEW_CONTROLLER_PERSIST_STATE_KEY @"ADHOC_VIEW_CONTROLLER_PERSIST_STATE_KEY"
+
 @interface AdhocViewController ()
 @property (weak, nonatomic) IBOutlet UITextField *trackTextField;
 @property (weak, nonatomic) IBOutlet UITextField *stateTextField;
@@ -24,6 +27,8 @@
     self.title = @"Adhoc";
     UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tap:)];
     [self.view addGestureRecognizer:tap];
+    self.trackTextField.text = [self retrieveLastUsedTrackString];
+    self.stateTextField.text = [self retrieveLastUsedStateString];
 }
 
 -(void)tap:(id)sender {
@@ -32,10 +37,12 @@
 
 - (IBAction)sendTrack:(id)sender {
     [Leanplum track:self.trackTextField.text];
+    [self persistLastUsedTrackString:self.trackTextField.text];
 }
 
 - (IBAction)sendState:(id)sender {
     [Leanplum advanceTo:self.stateTextField.text];
+    [self persistLastUsedStateString:self.stateTextField.text];
 }
 
 - (IBAction)setUserAttrib:(id)sender {
@@ -43,6 +50,26 @@
   @{
     self.userAttribTextField.text : self.userAttribValueTextField.text
     }];
+}
+
+-(void)persistLastUsedTrackString:(NSString *)track {
+    NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+    [userDefaults setObject:track forKey:ADHOC_VIEW_CONTROLLER_PERSIST_TRACK_KEY];
+}
+
+-(NSString *)retrieveLastUsedTrackString {
+    NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+    return [userDefaults objectForKey:ADHOC_VIEW_CONTROLLER_PERSIST_TRACK_KEY];
+}
+
+-(void)persistLastUsedStateString:(NSString *)state {
+    NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+    [userDefaults setObject:state forKey:ADHOC_VIEW_CONTROLLER_PERSIST_STATE_KEY];
+}
+
+-(NSString *)retrieveLastUsedStateString {
+    NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+    return [userDefaults objectForKey:ADHOC_VIEW_CONTROLLER_PERSIST_STATE_KEY];
 }
 
 @end
