040551cf856e2147cb41e6d2d77e57abc19b6471
Mayank Sanganeria <mayank.sanganeria@leanplum.com>
2020-01-07 15:53:13 -0800

    package

--- Tools/build.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-xcodebuild clean -workspace Rondo-iOS.xcworkspace -scheme Rondo-iOS
-xcodebuild archive -workspace Rondo-iOS.xcworkspace -scheme Rondo-iOS -archivePath build/Rondo-iOS.xcarchive
-xcodebuild -exportArchive -archivePath build/Rondo-iOS.xcarchive -exportPath build/ -exportOptionsPlist Rondo-iOS/ExportOptions.plist
