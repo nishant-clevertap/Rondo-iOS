927519ff632328b9d409d3f2d74c625bd6317d35
Mayank Sanganeria <mayank.sanganeria@leanplum.com>
2020-01-07 16:07:06 -0800

    fix path

--- Tools/package.sh
+++ Tools/package.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 xcodebuild clean -workspace Rondo-iOS.xcworkspace -scheme Rondo-iOS
 xcodebuild archive -workspace Rondo-iOS.xcworkspace -scheme Rondo-iOS -archivePath build/Rondo-iOS.xcarchive
-xcodebuild -exportArchive -archivePath build/Rondo-iOS.xcarchive -exportPath build/ -exportOptionsPlist Rondo-iOS/ExportOptions.plist
+xcodebuild -exportArchive -archivePath build/Rondo-iOS.xcarchive -exportPath build/ -exportOptionsPlist build/ExportOptions.plist
