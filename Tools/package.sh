c222a4a4dd64deb16fc66c6a95897655e4b5417b
Mayank Sanganeria <mayank.sanganeria@leanplum.com>
2020-01-07 16:01:17 -0800

    add package file

---
+++ Tools/package.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+xcodebuild clean -workspace Rondo-iOS.xcworkspace -scheme Rondo-iOS
+xcodebuild archive -workspace Rondo-iOS.xcworkspace -scheme Rondo-iOS -archivePath build/Rondo-iOS.xcarchive
+xcodebuild -exportArchive -archivePath build/Rondo-iOS.xcarchive -exportPath build/ -exportOptionsPlist Rondo-iOS/ExportOptions.plist
