853a4c9a0950242d87a14b12e173e8263eb1b809
Nikola Zagorchev <nikola.zagorchev@gmail.com>
2020-10-28 12:01:44 +0200

    Add defer IAM features

--- Rondo/UI/Messages/MessagesViewController.swift
+++ Rondo/UI/Messages/MessagesViewController.swift
@@ -15,7 +15,7 @@ class MessagesViewController: FormViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         LabelRow.defaultCellSetup = { cell, row in
             cell.selectionStyle = .default
             row.onCellSelection { (cell, row) in
@@ -53,16 +53,16 @@ class MessagesViewController: FormViewController {
         }
         UserDefaults.standard.useUNUserNotificationCenterDelegate = on
     }
-
+    
     func requestSystemPushPermission() {
         let center = UNUserNotificationCenter.current()
         center.requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
-
+            
             if let error = error {
                 // Handle the error here.
                 print("Error: \(error)")
             }
-
+            
             // Enable or disable features based on the authorization.
             if granted {
                 DispatchQueue.main.async {
@@ -71,15 +71,15 @@ class MessagesViewController: FormViewController {
             }
         }
     }
-
+    
     func addSegmentedControl() {
         segmentedControl.addTarget(self, action: #selector(MessagesViewController.didChangeSegment), for: .valueChanged)
         segmentedControl.selectedSegmentIndex = 0;
         segmentedControl.sizeToFit()
-
+        
         navigationItem.titleView = segmentedControl
     }
-
+    
     @objc func didChangeSegment() {
         title = segmentedControl.titleForSegment(at: segmentedControl.selectedSegmentIndex)
         if segmentedControl.selectedSegmentIndex == 0 {
@@ -88,52 +88,54 @@ class MessagesViewController: FormViewController {
             buildPush()
         }
     }
-
+    
     func buildIAM() {
         form.removeAll()
         buildTemplateMessages()
+        buildDeferIAMs()
         buildActions()
         buildRichMessages()
     }
-
+    
     func buildPush() {
         form.removeAll()
-
+        
         buildPushPermissions()
         buildSetPushDelegate()
         buildPushTriggers()
     }
+    
     func buildPushPermissions() {
         let section = Section("Push Permissions")
-
+        
         section <<< LabelRow {
             $0.title = "Push Permissions Through Leanplum"
             $0.tag = "registerPush"
         }
-
+        
         section <<< LabelRow {
             $0.title = "System Push Permission"
             $0.tag = "systemPush"
         }
-
+        
         form +++ section
     }
     
     func buildSetPushDelegate() {
-           let section = Section("Set push notification delegate")
-
-           section <<< SwitchRow {
-               $0.title = "UNUserNotificationCenterDelegate"
-               $0.tag = "setPushDelegate"
-               $0.value = UNUserNotificationCenter.current().delegate != nil
-           }
-
-           form +++ section
-       }
-
+        let section = Section("Set push notification delegate")
+        
+        section <<< SwitchRow {
+            $0.title = "UNUserNotificationCenterDelegate"
+            $0.tag = "setPushDelegate"
+            $0.value = UNUserNotificationCenter.current().delegate != nil
+        }
+        
+        form +++ section
+    }
+    
     func buildPushTriggers() {
         let section = Section("Push Notifications")
-
+        
         section <<< LabelRow {
             $0.title = "Push with Emoji"
             $0.tag = "pushRender"
@@ -174,13 +176,13 @@ class MessagesViewController: FormViewController {
             $0.title = "Local Push with Same Priority Different Time"
             $0.tag = "pushLocalSamePriorityDifferentTime"
         }
-
+        
         form +++ section
     }
-
+    
     func buildTemplateMessages() {
         let section = Section("In-App Messages")
-
+        
         section <<< LabelRow {
             $0.title = "Alert"
             $0.tag = "alert"
@@ -205,13 +207,116 @@ class MessagesViewController: FormViewController {
             $0.title = "Ads Pre-Permission"
             $0.tag = "adsAskToAsk"
         }
-
+        
         form +++ section
     }
-
+    
+    func buildDeferIAMs() {
+        let section = Section("Defer IAM")
+        section.tag = section.header?.title
+        
+        section <<< SwitchRow {
+            $0.title = "Defer IAM"
+            $0.tag = "deferIAM"
+            $0.value = UserDefaults.standard.bool(forKey: "deferIAM")
+        }.onChange { [weak self] (row) in
+            guard let self = self, let value = row.value else { return }
+            self.toggleDeferMessages(on: value)
+        }
+        
+        section <<< AccountRow {
+            $0.title = "Action Names"
+            $0.placeholder = "alert,confirm,html"
+        }
+        
+        section <<< AccountRow {
+            $0.title = "View Controllers"
+            $0.placeholder = "home,variables,inbox"
+        }
+        
+        section <<< ButtonRow {
+            $0.title = "Defer messages"
+        }.onCellSelection { (cell, row) in
+            
+            var names:[String] = []
+            if let row = section.allRows[1] as? AccountRow {
+                if let val = row.value {
+                names.append(contentsOf: val.components(separatedBy: ","))
+                }
+            }
+            var controllers:[String] = []
+            if let row = section.allRows[2] as? AccountRow {
+                if let val = row.value {
+                    controllers.append(contentsOf: val.components(separatedBy: ","))
+                }
+            }
+            self.deferMessages(names: names, forControllers: controllers)
+        }
+        
+        form +++ section
+    }
+    
+    private func deferMessages(names: [String], forControllers: [String]) {
+        if names.count > 0 {
+            var actionNames:[String] = []
+            
+            names.forEach {
+                let lc = $0.lowercased()
+                switch lc {
+                case "alert": actionNames.append("Alert")
+                case "confirm": actionNames.append("Confirm")
+                case "push": actionNames.append("Push Ask to Ask")
+                case "popup": actionNames.append("Center Popup")
+                case "int": actionNames.append("Interstitial")
+                case "webint": actionNames.append("Web Interstitial")
+                case "html": actionNames.append("HTML")
+                default: ()
+                }
+            }
+            
+            Leanplum.deferMessagesWithActionNames(actionNames)
+        }
+        if forControllers.count > 0 {
+            var controllerTypes:[AnyClass] = []
+            
+            forControllers.forEach {
+                let className = "Rondo_iOS.\(capitalizeFirstLetter(word: $0))ViewController"
+                    if let aClass = NSClassFromString(className) as? UIViewController.Type {
+                        controllerTypes.append(aClass)
+                    }
+            }
+            
+            if controllerTypes.count > 0 {
+                Leanplum.deferMessagesForViewControllers(controllerTypes)
+                
+                let section = form.sectionBy(tag: "Defer IAM")
+                let row = section?.first(where: {$0 as? SwitchRow != nil}) as! SwitchRow
+                row.value = true
+                row.reload()
+            }
+        }
+    }
+    
+    private func capitalizeFirstLetter(word:String) -> String {
+        let first = String(word.prefix(1)).capitalized
+                let other = String(word.dropFirst())
+                return first + other
+    }
+    
+    private func toggleDeferMessages(on: Bool) {
+        if on {
+            Leanplum.deferMessagesForViewControllers([HomeViewController.self])
+        } else {
+            Leanplum.deferMessagesWithActionNames([])
+            Leanplum.deferMessagesForViewControllers([])
+        }
+        UserDefaults.standard.set(on, forKey: "deferIAM")
+        //UserDefaults.standard.useUNUserNotificationCenterDelegate = on
+    }
+    
     func buildActions() {
         let section = Section("Actions")
-
+        
         section <<< LabelRow {
             $0.title = "Open URL"
             $0.tag = "openURL"
@@ -228,13 +333,13 @@ class MessagesViewController: FormViewController {
             $0.title = "Register for Ads Tracking"
             $0.tag = "registerAdsTracking"
         }
-
+        
         form +++ section
     }
-
+    
     func buildRichMessages() {
         let section = Section("Rich In-App Messaging")
-
+        
         section <<< LabelRow {
             $0.title = "Banner"
             $0.tag = "banner"
@@ -247,7 +352,7 @@ class MessagesViewController: FormViewController {
             $0.title = "Star Rating"
             $0.tag = "starRating"
         }
-
+        
         form +++ section
     }
 }
