b79a56535c166da3f783d99f089cfafb177f2b88
Nikola Zagorchev <nikola.zagorchev@gmail.com>
2021-04-19 11:40:20 +0300

    Update Automation app name

--- Rondo/AppContext.swift
+++ Rondo/AppContext.swift
@@ -52,7 +52,7 @@ class AppContext {
                                 appId: "app_ve9UCNlqI8dy6Omzfu1rEh6hkWonNHVZJIWtLLt6aLs",
                                 productionKey: "prod_D5ECYBLrRrrOYaFZvAFFHTg1JyZ2Llixe5s077Lw3rM",
                                 developmentKey: "dev_cKF5HMpLGqhbovlEGMKjgTuf8AHfr2Jar6rrnNhtzQ0"),
-                    LeanplumApp(name: "Rondo QA Automation",
+                    LeanplumApp(name: "Rondo Automation",
                                 appId: "app_UQcFGVeXzOCVsovrlUebad9R67hFJqzDegfQPZRnVZM",
                                 productionKey: "prod_lL8RSFzmHy0iVYXQpzjUVEHDlaUz5idT0H7BVs6Bn1Q",
                                 developmentKey: "dev_b9qX0tcazL5PCQFuZ7pxsfT6XHA7xQkaFtYVrgt4Kq0"),
