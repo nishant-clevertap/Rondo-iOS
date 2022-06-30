ac0de14ce68a7990c32d6b285c3749ed6720938e
Nikola Zagorchev <nikola.zagorchev@gmail.com>
2022-06-30 11:17:07 +0300

    Implement debug controller functionality

--- Rondo/UI/Debug/DebugViewController.swift
+++ Rondo/UI/Debug/DebugViewController.swift
@@ -3,7 +3,7 @@
 //  LPFeatures
 //
 //  Created by Milos Jakovljevic on 20/03/2020.
-//  Copyright © 2020 Leanplum. All rights reserved.
+//  Copyright © 2022 Leanplum. All rights reserved.
 //
 
 import UIKit
@@ -32,7 +32,7 @@ class DebugViewController: FormViewController {
             $0.cellStyle = .value1
             $0.title = "Messages"
             $0.presentationMode = .show(controllerProvider: .callback(builder: { () -> UIViewController in
-                return UIViewController()
+                ActionsController()
             }), onDismiss: nil)
         }
 
@@ -40,31 +40,21 @@ class DebugViewController: FormViewController {
             $0.cellStyle = .value1
             $0.title = "Vars"
             $0.presentationMode = .show(controllerProvider: .callback(builder: { () -> UIViewController in
-                return UIViewController()
+                let ctrl = AreaController()
+                ctrl.title = "Vars JSON"
+                ctrl.message = LPJSON.string(fromJSON: VarCache.shared().diffs())
+                return ctrl
             }), onDismiss: nil)
         }
 
         section <<< ButtonRow() {
             $0.cellStyle = .value1
-            $0.title = "Update rules"
+            $0.title = "Secured Vars"
             $0.presentationMode = .show(controllerProvider: .callback(builder: { () -> UIViewController in
-                return UIViewController()
-            }), onDismiss: nil)
-        }
-
-        section <<< ButtonRow() {
-            $0.cellStyle = .value1
-            $0.title = "Event rules"
-            $0.presentationMode = .show(controllerProvider: .callback(builder: { () -> UIViewController in
-                return UIViewController()
-            }), onDismiss: nil)
-        }
-
-        section <<< ButtonRow() {
-            $0.cellStyle = .value1
-            $0.title = "Event rules"
-            $0.presentationMode = .show(controllerProvider: .callback(builder: { () -> UIViewController in
-                return UIViewController()
+                let ctrl = AreaController()
+                ctrl.title = "Secured Vars JSON"
+                ctrl.message = LPJSON.string(fromJSON: VarCache.shared().securedVars())
+                return ctrl
             }), onDismiss: nil)
         }
 
@@ -72,7 +62,12 @@ class DebugViewController: FormViewController {
             $0.cellStyle = .value1
             $0.title = "Variants"
             $0.presentationMode = .show(controllerProvider: .callback(builder: { () -> UIViewController in
-                return UIViewController()
+                let ctrl = AreaController()
+                ctrl.title = "Variants JSON"
+                ctrl.message = "Not supported yet"
+//                let variants = VarCache.shared().variants()
+//                ctrl.message = LPJSON.string(fromJSON: variants)
+                return ctrl
             }), onDismiss: nil)
         }
 
@@ -80,10 +75,87 @@ class DebugViewController: FormViewController {
             $0.cellStyle = .value1
             $0.title = "Regions"
             $0.presentationMode = .show(controllerProvider: .callback(builder: { () -> UIViewController in
-                return UIViewController()
+                let ctrl = AreaController()
+                ctrl.title = "Regions JSON"
+                ctrl.message = LPJSON.string(fromJSON: VarCache.shared().regions())
+                return ctrl
             }), onDismiss: nil)
         }
 
+        form +++ section
+        
+        let resetSection = Section("Reset")
+        
+        resetSection <<< ButtonRow {
+            $0.title = "Delete events data"
+        }.onCellSelection({ (cell, row) in
+            LPEventDataManager.deleteEvents(withLimit: LPEventDataManager.count())
+        })
+        
+        resetSection <<< ButtonRow {
+            $0.title = "Reset"
+        }.onCellSelection({ (cell, row) in
+            // Calls private static reset method
+            Leanplum.perform(NSSelectorFromString("reset"))
+        })
+        
+        form +++ resetSection
+    }
+}
+
+class ActionsController: FormViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        build()
+    }
+    
+    func build() {
+        let section = Section(title)
+        
+        let messages = ActionManager.shared.messages
+        
+        for (key, value) in messages {
+            let config = value as? [AnyHashable: Any]
+            let actionName = config?[LP_PARAM_ACTION] ?? "Unknown"
+            let title = "\(key): \(actionName)"
+            section <<< ButtonRow() { (row) in
+                row.title = title
+                row.cellStyle = .value1
+                row.value = key as? String
+                row.presentationMode = .show(controllerProvider: .callback(builder: { () -> UIViewController in
+                    let ctrl = AreaController()
+                    ctrl.title = row.title
+                    ctrl.message = LPJSON.string(fromJSON: ActionManager.shared.messages[row.value!])
+                    return ctrl
+                }), onDismiss: nil)
+                row.displayValueFor = {
+                    return $0
+                }
+            }
+        }
+
+        form +++ section
+    }
+}
+
+class AreaController: FormViewController {
+    var message: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        build()
+    }
+    
+    func build() {
+        let section = Section(title)
+
+        section <<< TextAreaRow() {
+            $0.value = message
+            $0.textAreaHeight = .dynamic(initialTextViewHeight: 96)
+        }
+        
         form +++ section
     }
 }
