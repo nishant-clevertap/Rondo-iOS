15970ed67a7297f14ed3f506aa81bbfd27e47ee2
Nishant Kumar <nishant.kumar@clevertap.com>
2024-08-07 16:23:56 +0530

    - Contains only AppDelegate file.

--- Rondo/AppDelegate.swift
+++ Rondo/AppDelegate.swift
@@ -15,6 +15,7 @@ import CleverTapSDK
 class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
     
     let context = AppContext()
+    var window: UIWindow?
     
     var notificationOptions: UNNotificationPresentationOptions = []
 
@@ -43,21 +44,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
             Log.print("Unexpected error: \(error).")
         }
         
+        window?.rootViewController = TabViewController()
+        window?.makeKeyAndVisible()
+        
         return true
-    }
-    
-    // MARK: UISceneSession Lifecycle
-    
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-    
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
     
     func application(_ application: UIApplication,
